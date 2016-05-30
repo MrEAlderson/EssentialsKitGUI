be05088618a4496a08561babab3f6291c81b8049
Marcely1199 <Mylogan99@web.de>
2016-05-30 16:46:51 +0200

    Update 1.3

--- src/de/marcely/kitgui/main.java
+++ src/de/marcely/kitgui/main.java
@@ -1,3 +1,12 @@
+/**
+* Adds an GUI for the essentials command /kit
+* https://www.spigotmc.org/resources/essentials-kit-gui-opensource.15160/
+*
+* @author  Marcely1199
+* @version 1.3
+* @website http://marcely.de/ 
+*/
+
 package de.marcely.kitgui;
 
 import java.io.File;
@@ -5,34 +14,36 @@ import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.marcely.kitgui.command.kit;
 import de.marcely.kitgui.config.KitConfig;
-import de.marcely.kitgui.config.config;
+import de.marcely.kitgui.config.Config;
+import de.marcely.kitgui.config.LanguageConfig;
 import net.ess3.api.IEssentials;
 
-
 public class main extends JavaPlugin {
-	public static String version = "1.3";
+	public static Plugin plugin;
 	
 	public static IEssentials es = null;
 	
 	public static String CONFIG_INVTITLE = ChatColor.DARK_AQUA + "Kits";
 	public static boolean CONFIG_FIRSTCHARCAPS = false;
-	public static LanguageType CONFIG_LANGUAGE = LanguageType.English;
+	public static boolean CONFIG_INCLCMD_KITS = true;
 	
 	public static KitConfig kits = new KitConfig();
 	
-	@Override
 	public void onEnable(){
+		plugin = this;
+		
 		// get essentials variable
 		es = (IEssentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials");
 		if(es == null) // if he isn't using the spigot version of spigot
@@ -49,38 +60,45 @@ public class main extends JavaPlugin {
 		File dir = new File("plugins/Essentials_KitGUI");
 		if(!dir.exists()) dir.mkdir();
 		
-		config.load();
+		Config.load();
+		LanguageConfig.load();
 		if(KitConfig.exists()) kits = KitConfig.load();
-	}
-	
-	@Override
-	public void onDisable(){
-		
 	}
 	
 	private Listener listener = new Listener(){
 		@EventHandler
 		public void onInventoryClick(InventoryClickEvent event){
-			kit.onInventoryClick(event);
+			kit.onInventoryClickEvent(event);
+		}
+		
+		@EventHandler
+		public void onInventoryDragEvent(InventoryDragEvent event){
+			kit.onInventoryDragEvent(event);
+		}
+		
+		@EventHandler
+		public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent event){
+			String[] strs = event.getMessage().split(" ");
+			String label = strs[0].replace("/", "");
+			String[] args = new String[strs.length - 1];
+			
+			for(int i=1; i<strs.length; i++)
+				args[i - 1] = strs[i];
+			
+			if(CONFIG_INCLCMD_KITS && label.equalsIgnoreCase("kits")){
+				kit.onCommand(event.getPlayer(), label, args);
+				event.setCancelled(true);
+			}
 		}
 	};
 	
 	public static ArrayList<Kit> getKits(Player player){
 		ArrayList<Kit> list = new ArrayList<Kit>();
-		ConfigurationSection kits = es.getSettings().getKits();
-		for (String kitItem:kits.getKeys(false)){
-			try {
-				com.earth2me.essentials.Kit kit = new com.earth2me.essentials.Kit(kitItem, es);
-				if(player.hasPermission("essentials.kits." + kit.getName())){
-					Kit k = main.kits.getKit(kit.getName());
-					if(k == null)
-						list.add(new Kit(kit.getName(), new ItemStack(Material.CLAY_BALL)));
-					else
-						list.add(new Kit(k.getName(), k.getIcon(), k.getPrefix()));
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		for(String kitName:es.getSettings().getKits().getKeys(false)){
+			
+			Kit kit = kits.getKit(kitName);
+			list.add(kit);
+			
 		}
 		return list;
 	}
@@ -90,7 +108,7 @@ public class main extends JavaPlugin {
 		for (String kitItem:kits.getKeys(false)){
 			try {
 				com.earth2me.essentials.Kit kit = new com.earth2me.essentials.Kit(kitItem, es);
-				if(kit.getName().equals(kitname)){
+				if(kit.getName().equalsIgnoreCase(kitname)){
 					return kit;
 				}
 			} catch (Exception e) {
@@ -121,5 +139,18 @@ public class main extends JavaPlugin {
 		if(CONFIG_FIRSTCHARCAPS == true)
 			return Character.toUpperCase(str.charAt(0)) + str.substring(1);
 		return str;
+	}
+	
+	public static boolean isInteger(String str){
+		try{
+			Integer.valueOf(str);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static String getVersion(){
+		return plugin.getDescription().getVersion();
 	}
 }
